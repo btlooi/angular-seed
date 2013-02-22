1374f8688a52e46e4821d3a9df35c9869612e3e5
BTLooi <looibt@gmail.com>
2013-02-22 14:47:39 +0800

    2

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -44,3 +44,24 @@ describe('MyApp controllers', function() {
   });
 });
 
+
+describe('MyApp App', function() {
+ 
+  describe('Phone list view', function() {
+ 
+    beforeEach(function() {
+      browser().navigateTo('../../app/index.html');
+    });
+ 
+ 
+    it('should filter the phone list as user types into the search box', function() {
+      expect(repeater('.phones li').count()).toBe(3);
+ 
+      input('query').enter('nexus');
+      expect(repeater('.phones li').count()).toBe(1);
+ 
+      input('query').enter('motorola');
+      expect(repeater('.phones li').count()).toBe(2);
+    });
+  });
+});
